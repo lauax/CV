077e807c751e2fa05faf11aaa43823e1ee5def44
lauax <lucas12alfredsson@gmail.com>
2022-11-28 16:57:17 +0100

    Början för mitt CV

---
+++ main.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', main)
+
+
+
+function main() {
+   LightTheme()
+}
+
+function DarkTheme(){
+    document.getElementById("gif-image").style.backgroundImage = "url('gif-bild3.png')";
+
+}
+function LightTheme(){
+    document.getElementById("gif-image").style.backgroundImage = "url('gif-bild.png')";
+
+}
