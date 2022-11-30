bdca948ae9839bfe6d915756e87819db2a3700bc
lauax <lucas12alfredsson@gmail.com>
2022-11-30 17:22:37 +0100

    Färdigställt sectionen Skills

--- main.js
+++ main.js
@@ -14,3 +14,31 @@ function LightTheme(){
     document.getElementById("gif-image").style.backgroundImage = "url('gif-bild.png')";
 
 }
+
+function SkillsTag1(){
+    var toggle = document.getElementById("skills-p-tag1");
+    if (toggle.style.visibility === "hidden") {
+      toggle.style.visibility = "visible";
+    } else {
+      toggle.style.visibility = "hidden";
+    }
+}
+
+function SkillsTag2(){
+    var toggle = document.getElementById("skills-p-tag2");
+    if (toggle.style.visibility === "hidden") {
+      toggle.style.visibility = "visible";
+    } else {
+      toggle.style.visibility = "hidden";
+    }
+}
+
+function SkillsTag3(){
+    var toggle = document.getElementById("skills-p-tag3");
+  if (toggle.style.visibility === "hidden") {
+    toggle.style.visibility = "visible";
+  } else {
+    toggle.style.visibility = "hidden";
+  }
+}
+
